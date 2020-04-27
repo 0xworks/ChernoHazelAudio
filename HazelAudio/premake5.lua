48f982cf27dfd0140cc3e1e5b6caba7091248d6e
freeman40 <jrfwaite@gmail.com>
2020-04-27 21:27:58 +1200

    Updated premake files slightly to make HazelAudio easier to use as a submodule in third party projects

---
+++ HazelAudio/premake5.lua
@@ -0,0 +1,53 @@
+project "HazelAudio"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"AL_LIBTYPE_STATIC"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"HazelAudio/vendor/OpenAL-Soft/include",
+		"HazelAudio/vendor/libogg/include",
+		"HazelAudio/vendor/Vorbis/include",
+		"HazelAudio/vendor/minimp3"
+	}
+
+	links
+	{
+		"OpenAL-Soft",
+		"Vorbis"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
